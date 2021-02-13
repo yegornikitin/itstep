03bef9c409efb1cae1a44817fe399f4ab2c26a53
yegornikitin <78050881+yegornikitin@users.noreply.github.com>
2021-02-13 19:13:19 +0200

    Delete text updated.py

--- lesson7/text updated.py
+++
@@ -1,9 +0,0 @@
-text = input("Please, write something: \n")
-print("Number of sentences: ", len(text.split(".")))
-a = [".", ",", "?", "!"]
-for i in set(text):
-    if i in a:
-        print("Число знаков препинания: ", i, text.count(i))
-
-
-
